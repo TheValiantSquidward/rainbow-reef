f8d489ca3ab2401526224e6b5712cdfe7074abe9
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2025-12-16 22:35:44 -0700

    Update Billfish.java

--- src/main/java/net/thevaliantsquidward/rainbowreef/entity/Billfish.java
+++ src/main/java/net/thevaliantsquidward/rainbowreef/entity/Billfish.java
@@ -13,7 +13,6 @@ import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl;
-import net.minecraft.world.entity.ai.control.SmoothSwimmingMoveControl;
 import net.minecraft.world.entity.ai.goal.TryFindWaterGoal;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.item.ItemStack;
