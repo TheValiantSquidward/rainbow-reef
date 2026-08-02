844a9b539b2f744e3ad83d168286d17a6cfa2ba9
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2026-08-01 22:58:38 -0600

    some spawning stuff

--- src/main/java/com/valiantenvoy/rainbow_reef/datagen/ReefBiomeModifiers.java
+++ src/main/java/com/valiantenvoy/rainbow_reef/datagen/ReefBiomeModifiers.java
@@ -19,27 +19,31 @@ import java.util.function.Supplier;
 public class ReefBiomeModifiers {
 
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
-        addSpawn(context, "angelfish", ReefBiomeTags.HAS_ANGELFISH, new MobSpawnSettings.SpawnerData(ReefEntities.ANGELFISH.get(), 10, 2, 2));
-        addSpawn(context, "arrow_crab", ReefBiomeTags.HAS_ARROW_CRAB, new MobSpawnSettings.SpawnerData(ReefEntities.ARROW_CRAB.get(), 10, 2, 2));
-        addSpawn(context, "basslet", ReefBiomeTags.HAS_BASSLET, new MobSpawnSettings.SpawnerData(ReefEntities.BASSLET.get(), 15, 6, 6));
-        addSpawn(context, "boxfish", ReefBiomeTags.HAS_BOXFISH, new MobSpawnSettings.SpawnerData(ReefEntities.BOXFISH.get(), 20, 3, 3));
-        addSpawn(context, "butterflyfish", ReefBiomeTags.HAS_BUTTERFLYFISH, new MobSpawnSettings.SpawnerData(ReefEntities.BUTTERFLYFISH.get(), 40, 2, 2));
-        addSpawn(context, "butterflyfish_mangrove", ReefBiomeTags.HAS_BUTTERFLYFISH_MANGROVE, new MobSpawnSettings.SpawnerData(ReefEntities.BUTTERFLYFISH.get(), 20, 1, 1));
-        addSpawn(context, "clownfish", ReefBiomeTags.HAS_CLOWNFISH, new MobSpawnSettings.SpawnerData(ReefEntities.CLOWNFISH.get(), 20, 2, 2));
+        addSpawn(context, "angelfish", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.ANGELFISH.get(), 10, 4, 4));
+        addSpawn(context, "arrow_crab", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.ARROW_CRAB.get(), 10, 2, 2));
+        addSpawn(context, "basslet", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.BASSLET.get(), 15, 6, 6));
+        addSpawn(context, "boxfish", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.BOXFISH.get(), 20, 3, 3));
+        addSpawn(context, "butterflyfish", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.BUTTERFLYFISH.get(), 40, 2, 2));
+        addSpawn(context, "butterflyfish_mangrove", ReefBiomeTags.HAS_BUTTERFLYFISH_MANGROVE, new MobSpawnSettings.SpawnerData(ReefEntities.BUTTERFLYFISH.get(), 20, 12, 12));
+        addSpawn(context, "clownfish", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.CLOWNFISH.get(), 20, 4, 4));
         addSpawn(context, "crab", ReefBiomeTags.HAS_CRAB, new MobSpawnSettings.SpawnerData(ReefEntities.CRAB.get(), 20, 2, 2));
-        addSpawn(context, "dwarf_angelfish", ReefBiomeTags.HAS_DWARF_ANGELFISH, new MobSpawnSettings.SpawnerData(ReefEntities.DWARF_ANGELFISH.get(), 30, 4, 4));
-        addSpawn(context, "goby", ReefBiomeTags.HAS_GOBY, new MobSpawnSettings.SpawnerData(ReefEntities.GOBY.get(), 35, 5, 5));
+        addSpawn(context, "dwarf_angelfish", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.DWARF_ANGELFISH.get(), 30, 6, 6));
+        addSpawn(context, "goby", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.GOBY.get(), 30, 5, 5));
         addSpawn(context, "goby_mangrove", ReefBiomeTags.HAS_GOBY_MANGROVE, new MobSpawnSettings.SpawnerData(ReefEntities.GOBY.get(), 15, 3, 3));
-        addSpawn(context, "hogfish", ReefBiomeTags.HAS_HOGFISH, new MobSpawnSettings.SpawnerData(ReefEntities.HOGFISH.get(), 10, 3, 3));
+        addSpawn(context, "hogfish", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.HOGFISH.get(), 15, 5, 5));
         addSpawn(context, "jellyfish", ReefBiomeTags.HAS_JELLYFISH, new MobSpawnSettings.SpawnerData(ReefEntities.JELLYFISH.get(), 8, 3, 3));
         addSpawn(context, "jellyfish_rare", ReefBiomeTags.HAS_JELLYFISH_RARE, new MobSpawnSettings.SpawnerData(ReefEntities.JELLYFISH.get(), 4, 2, 2));
-        addSpawn(context, "moorish_idol", ReefBiomeTags.HAS_MOORISH_IDOL, new MobSpawnSettings.SpawnerData(ReefEntities.MOORISH_IDOL.get(), 8, 1, 1));
-        addSpawn(context, "parrotfish", ReefBiomeTags.HAS_PARROTFISH, new MobSpawnSettings.SpawnerData(ReefEntities.PARROTFISH.get(), 12, 1, 1));
+        addSpawn(context, "moorish_idol", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.MOORISH_IDOL.get(), 10, 10, 10));
+        addSpawn(context, "parrotfish", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.PARROTFISH.get(), 15, 5, 5));
         addSpawn(context, "pipefish", ReefBiomeTags.HAS_PIPEFISH, new MobSpawnSettings.SpawnerData(ReefEntities.PIPEFISH.get(), 10, 3, 3));
-        addSpawn(context, "ray", ReefBiomeTags.HAS_RAY, new MobSpawnSettings.SpawnerData(ReefEntities.RAY.get(), 2, 1, 1));
-        addSpawn(context, "seahorse", ReefBiomeTags.HAS_SEAHORSE, new MobSpawnSettings.SpawnerData(ReefEntities.SEAHORSE.get(), 8, 4, 4));
-        addSpawn(context, "small_shark", ReefBiomeTags.HAS_SMALL_SHARK, new MobSpawnSettings.SpawnerData(ReefEntities.SMALL_SHARK.get(), 10, 3, 3));
-        addSpawn(context, "tang", ReefBiomeTags.HAS_TANG, new MobSpawnSettings.SpawnerData(ReefEntities.TANG.get(), 40, 20, 20));
+        addSpawn(context, "ray", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.RAY.get(), 5, 3, 3));
+        addSpawn(context, "seahorse", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.SEAHORSE.get(), 10, 4, 4));
+        addSpawn(context, "small_shark", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.SMALL_SHARK.get(), 10, 3, 3));
+        addSpawn(context, "tang", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.TANG.get(), 40, 20, 20));
+        addSpawn(context, "damselfish", ReefBiomeTags.WARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.DAMSELFISH.get(), 30, 24, 24));
+        addSpawn(context, "fusilier", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.FUSILIER.get(), 25, 16, 16));
+        addSpawn(context, "shark", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.SHARK.get(), 5, 3, 3));
+        addSpawn(context, "large_shark", ReefBiomeTags.WARM_AND_LUKEWARM_OCEANS, new MobSpawnSettings.SpawnerData(ReefEntities.LARGE_SHARK.get(), 2, 1, 1));
     }
 
     private static void addSpawn(BootstrapContext<BiomeModifier> context, String name, TagKey<Biome> biomes, MobSpawnSettings.SpawnerData... spawns) {
